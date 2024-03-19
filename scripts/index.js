a56b47dd9b5db334f000dc35a66fa5dbe4062773
Shelton <sheltonbrockett@gmail.com>
2024-03-18 21:41:01 -0400

    Project 4 Stage 3 Submission 1

--- scripts/index.js
+++ scripts/index.js
@@ -30,15 +30,56 @@ const initialCards = [
   },
 ];
 
+/* Elements */
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile_edit-modal");
+const modalCloseButton = document.querySelector("#modal-button-close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
+/* Functions  */
+function closePopUp() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardElementImage = cardElement.querySelector(".card__image");
+  const cardElementTitle = cardElement.querySelector(".card__title");
+  cardElementTitle.textContent = data.name;
+  cardElementImage.alt = data.name;
+  cardElementImage.src = data.link;
+  return cardElement;
+}
+
+/* Event Listeners */
 profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
 });
 
-const modalCloseButton = document.querySelector("#modal-button-close");
-
 modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+  closePopUp();
 });
+
+profileEditForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopUp();
+});
+
+for (i = 0; i <= initialCards.length; i++) {
+  const data = initialCards[i];
+  const cardElement = getCardElement(data);
+  cardList.prepend(cardElement);
+}
